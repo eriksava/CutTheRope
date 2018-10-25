88bb5e51378f64266420853d913f85576b8e2dd5
Erik S <eriksava@hotmail.com>
2018-10-25 11:18:53 -0700

    base logic

---
+++ Assets/Scripts/CutScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutScript : MonoBehaviour
+{
+
+    public Camera cam;
+
+
+
+
+    private void Update()
+    {
+        if(Input.GetMouseButton(0)){
+        RaycastHit2D ray = Physics2D.Raycast(cam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+        if(ray.collider != null){
+            if(ray.collider.tag == "Rope"){
+                Destroy(ray.collider.gameObject);
+            }
+          }
+        }
+    }
+
+}
+
