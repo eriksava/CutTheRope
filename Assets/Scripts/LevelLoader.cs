241cf45f9f96bce0e21718c41ed2453c375a33a1
Erik S <eriksava@hotmail.com>
2018-10-26 12:17:03 -0700

    game playable

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Candy"){
+            SceneManager.LoadScene("Scene2");
+        }
+    }
+}
